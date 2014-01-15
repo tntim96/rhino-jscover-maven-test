fde182fa8973eeefcb3a2deaf252306555d25eb4
tntim96 <tntim96@gmail.com>
2014-01-15 21:16:11 +1100

    First commit

---
+++ rhino-bundle.sh
@@ -0,0 +1,10 @@
+cd ${M2_REPO}/com/github/tntim96/rhino/1.7R5pre01/
+jar cvf rhino-1.7R5pre01-bundle.jar \
+	rhino-1.7R5pre01.jar \
+	rhino-1.7R5pre01.pom \
+	rhino-1.7R5pre01-javadoc.jar \
+	rhino-1.7R5pre01-sources.jar \
+	rhino-1.7R5pre01.jar.asc \
+	rhino-1.7R5pre01.pom.asc \
+	rhino-1.7R5pre01-javadoc.jar.asc \
+	rhino-1.7R5pre01-sources.jar.asc
